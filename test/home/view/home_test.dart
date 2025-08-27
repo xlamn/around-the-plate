40540d331546104fed52be7758b11c396958fe6e
TuanLamNguyen <xnguyen.lam@gmail.com>
2025-08-28 00:10:12 +0800

    test: update home_test.dart.

--- test/home/view/home_test.dart
+++ test/home/view/home_test.dart
@@ -2,12 +2,16 @@ import 'package:around_the_plate/features/dishes_overview/view/dishes_overview_p
 import 'package:around_the_plate/features/map_overview/view/map_overview_page.dart';
 import 'package:around_the_plate/features/settings_overview/view/settings_overview_page.dart';
 import 'package:around_the_plate/home/home.dart';
+import 'package:bloc_test/bloc_test.dart';
 import 'package:dishes_repository/dishes_repository.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:forui/forui.dart';
 import 'package:mocktail/mocktail.dart';
 
 import '../../helpers/helpers.dart';
+
+class MockHomeCubit extends MockCubit<int> implements HomeCubit {}
 
 void main() {
   late DishesRepository dishesRepository;
@@ -29,9 +33,30 @@ void main() {
   });
 
   group('`$HomeView`', () {
-    testWidgets('renders `$DishesOverviewPage` initially', (tester) async {
+    late HomeCubit cubit;
+
+    setUp(() {
+      cubit = MockHomeCubit();
+      when(() => cubit.state).thenReturn(0);
+
+      dishesRepository = MockDishesRepository();
+      when(dishesRepository.getDishes).thenAnswer((_) => const Stream.empty());
+    });
+
+    Widget buildSubject() {
+      return BlocProvider.value(
+        value: cubit,
+        child: HomeView(),
+      );
+    }
+
+    testWidgets('renders `$DishesOverviewPage` when tab is set to 0', (
+      tester,
+    ) async {
+      when(() => cubit.state).thenReturn(0);
+
       await tester.pumpApp(
-        const Home(),
+        buildSubject(),
         dishesRepository: dishesRepository,
       );
 
@@ -40,28 +65,30 @@ void main() {
       expect(find.byType(SettingsOverviewPage), findsNothing);
     });
 
-    testWidgets('renders `$MapOverviewPage`', (tester) async {
+    testWidgets('renders `$MapOverviewPage` when tab is set to 1', (
+      tester,
+    ) async {
+      when(() => cubit.state).thenReturn(1);
+
       await tester.pumpApp(
-        const Home(),
+        buildSubject(),
         dishesRepository: dishesRepository,
       );
-
-      await tester.tap(find.byIcon(FIcons.map));
-      await tester.pump(const Duration(seconds: 1));
 
       expect(find.byType(DishesOverviewPage), findsNothing);
       expect(find.byType(MapOverviewPage), findsOneWidget);
       expect(find.byType(SettingsOverviewPage), findsNothing);
     });
 
-    testWidgets('renders `$SettingsOverviewPage`', (tester) async {
+    testWidgets('renders `$SettingsOverviewPage` when tab is set to 2', (
+      tester,
+    ) async {
+      when(() => cubit.state).thenReturn(2);
+
       await tester.pumpApp(
-        const Home(),
+        buildSubject(),
         dishesRepository: dishesRepository,
       );
-
-      await tester.tap(find.byIcon(FIcons.settings));
-      await tester.pump(const Duration(seconds: 1));
 
       expect(find.byType(DishesOverviewPage), findsNothing);
       expect(find.byType(MapOverviewPage), findsNothing);
