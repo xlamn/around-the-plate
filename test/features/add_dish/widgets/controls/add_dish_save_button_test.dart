f710baa8dd39114029b066149ed42fdaf7c9da21
TuanLamNguyen <xnguyen.lam@gmail.com>
2025-09-13 15:11:30 +0700

    test: update add_dish_save_button_test.dart.

--- test/features/add_dish/widgets/controls/add_dish_save_button_test.dart
+++ test/features/add_dish/widgets/controls/add_dish_save_button_test.dart
@@ -1,43 +1,30 @@
-import 'package:around_the_plate/features/add_dish/cubits/add_dish/add_dish_cubit.dart';
-import 'package:around_the_plate/features/add_dish/cubits/add_dish/add_dish_state.dart';
 import 'package:around_the_plate/features/add_dish/widgets/add_dish_save_button.dart';
-import 'package:bloc_test/bloc_test.dart';
-import 'package:dishes_repository/dishes_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:forui/forui.dart';
 import 'package:mocktail/mocktail.dart';
 
 import '../../../../helpers/helpers.dart';
 
-class MockAddDishCubit extends MockBloc<AddDishCubit, AddDishState>
-    implements AddDishCubit {}
+class MockFunction extends Mock {
+  Future<void> onPressed();
+}
 
 void main() {
-  late AddDishCubit addDishCubit;
-  late Dish dish;
+  late MockFunction functions;
 
   setUp(() {
-    addDishCubit = MockAddDishCubit();
-    dish = Dish(
-      name: 'Test Dish',
-      imagePath: '/path/to/image.jpg',
-      rating: 4.5,
-    );
-    when(() => addDishCubit.addDish(dish)).thenAnswer((_) => Future.value());
+    functions = MockFunction();
+    when(() => functions.onPressed()).thenAnswer((_) => Future.value());
   });
 
   Widget buildSubject() {
-    return BlocProvider<AddDishCubit>.value(
-      value: addDishCubit,
-      child: AddDishSaveButton(
-        getDish: () => dish,
-      ),
+    return AddDishSaveButton(
+      onPressed: functions.onPressed,
     );
   }
 
-  group('AddDishSaveButton', () {
+  group('`$AddDishSaveButton`', () {
     testWidgets('renders correctly', (tester) async {
       await tester.pumpApp(buildSubject());
 
@@ -45,7 +32,7 @@ void main() {
       expect(find.text('Add Dish'), findsOneWidget);
     });
 
-    testWidgets('calls cubit.addDish when pressed', (
+    testWidgets('calls correct function when pressed', (
       tester,
     ) async {
       await tester.pumpApp(buildSubject());
@@ -53,7 +40,7 @@ void main() {
       await tester.tap(find.byType(AddDishSaveButton));
       await tester.pump(Duration(seconds: 1));
 
-      verify(() => addDishCubit.addDish(dish)).called(1);
+      verify(() => functions.onPressed()).called(1);
     });
   });
 }
