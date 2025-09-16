92e91df9901f9de64be0b2cce2c46dcf92305f31
TuanLamNguyen <xnguyen.lam@gmail.com>
2025-09-16 11:36:30 +0700

    test: update app_test.dart.

--- test/app/app_test.dart
+++ test/app/app_test.dart
@@ -1,5 +1,8 @@
 import 'package:around_the_plate/app/app.dart';
+import 'package:around_the_plate/app/cubits/app_startup_cubit.dart';
 import 'package:around_the_plate/features/home/view/home.dart';
+import 'package:around_the_plate/features/onboarding/views/onboarding_flow.dart';
+import 'package:bloc_test/bloc_test.dart';
 import 'package:dishes_repository/dishes_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,42 +11,61 @@ import 'package:mocktail/mocktail.dart';
 
 import '../helpers/pump_app.dart';
 
+class MockAppStartupCubit extends MockBloc<AppStartupCubit, AppStartupState>
+    implements AppStartupCubit {}
+
 void main() {
   late DishesRepository dishesRepository;
+  late AppStartupCubit appStartupCubit;
 
   setUp(() {
+    appStartupCubit = MockAppStartupCubit();
+    when(() => appStartupCubit.state).thenReturn(AppStartupState.loading());
     dishesRepository = MockDishesRepository();
     when(
       () => dishesRepository.getDishes(),
     ).thenAnswer((_) => const Stream.empty());
   });
 
-  group('App', () {
+  group('`$App`', () {
     testWidgets('renders AppView', (tester) async {
       await tester.pumpWidget(
         App(createDishesRepository: () => dishesRepository),
       );
 
+      expect(find.byType(MaterialApp), findsOneWidget);
       expect(find.byType(AppView), findsOneWidget);
     });
   });
 
-  group('AppView', () {
-    testWidgets('renders MaterialApp', (tester) async {
-      await tester.pumpWidget(
-        RepositoryProvider.value(
-          value: dishesRepository,
+  group('`$AppView`', () {
+    testWidgets('renders $OnboardingFlow when onboarding not completed', (
+      tester,
+    ) async {
+      when(
+        () => appStartupCubit.state,
+      ).thenReturn(AppStartupState.loaded(false));
+
+      await tester.pumpApp(
+        dishesRepository: dishesRepository,
+        BlocProvider.value(
+          value: appStartupCubit,
           child: const AppView(),
         ),
       );
 
-      expect(find.byType(MaterialApp), findsOneWidget);
+      expect(find.byType(OnboardingFlow), findsOneWidget);
     });
 
-    testWidgets('renders Home', (tester) async {
-      await tester.pumpWidget(
-        RepositoryProvider.value(
-          value: dishesRepository,
+    testWidgets('renders $Home when onboarding completed', (tester) async {
+      when(
+        () => appStartupCubit.state,
+      ).thenReturn(AppStartupState.loaded(true));
+
+      await tester.pumpApp(
+        dishesRepository: dishesRepository,
+        BlocProvider.value(
+          value: appStartupCubit,
           child: const AppView(),
         ),
       );
