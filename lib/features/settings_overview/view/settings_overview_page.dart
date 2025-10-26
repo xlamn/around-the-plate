94c15ef66da9ccb9b3be115ba70b89c50e1ffece
TuanLamNguyen <xnguyen.lam@gmail.com>
2025-10-26 15:31:49 +0700

    settings page first structure.

--- lib/features/settings_overview/view/settings_overview_page.dart
+++ lib/features/settings_overview/view/settings_overview_page.dart
@@ -10,26 +10,70 @@ class SettingsOverviewPage extends StatelessWidget {
       children: [
         FHeader(
           title: const Text('Settings'),
-          suffixes: [
-            FHeaderAction(
-              icon: Icon(FIcons.ellipsis),
-              onPress: () {},
-            ),
-          ],
         ),
-        const SizedBox(height: 5),
-        FCard(
-          title: const Text('Account'),
-          subtitle: const Text(
-            'Make changes to your account here. Click save when you are done.',
-          ),
-          child: Column(
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: FTileGroup(
+            label: const Text('General'),
             children: [
-              const FTextField(label: Text('Name'), hint: 'John Renalo'),
-              const SizedBox(height: 10),
-              const FTextField(label: Text('Email'), hint: 'john@doe.com'),
-              const SizedBox(height: 16),
-              FButton(onPress: () {}, child: const Text('Save')),
+              FTile(
+                prefix: Icon(FIcons.palette),
+                title: const Text('Theme'),
+                suffix: Icon(FIcons.chevronRight),
+                onPress: () {},
+              ),
+              FTile(
+                prefix: Icon(FIcons.refreshCcwDot),
+                title: const Text('Synchronization'),
+                suffix: Icon(FIcons.chevronRight),
+                onPress: () {},
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: FTileGroup(
+            label: const Text('Support'),
+            children: [
+              FTile(
+                prefix: Icon(FIcons.mail),
+                title: const Text('Contact Developer'),
+                suffix: Icon(FIcons.squareArrowOutUpRight),
+                onPress: () {},
+              ),
+              FTile(
+                prefix: Icon(FIcons.cookie),
+                title: const Text('Buy a cookie'),
+                suffix: Icon(FIcons.squareArrowOutUpRight),
+                onPress: () {},
+              ),
+              FTile(
+                prefix: Icon(FIcons.star),
+                title: const Text('Rate App'),
+                suffix: Icon(FIcons.squareArrowOutUpRight),
+                onPress: () {},
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: FTileGroup(
+            label: const Text('Legal'),
+            children: [
+              FTile(
+                prefix: Icon(FIcons.fileSpreadsheet),
+                title: const Text('Imprint'),
+                suffix: Icon(FIcons.chevronRight),
+                onPress: () {},
+              ),
+              FTile(
+                prefix: Icon(FIcons.handshake),
+                title: const Text('Privacy Policy'),
+                suffix: Icon(FIcons.chevronRight),
+                onPress: () {},
+              ),
             ],
           ),
         ),
